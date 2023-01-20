ea6e07406b1dea4284a2dc20c86cd13668f46ea2
Zayyad Muhammad Sani <71462377+Z-MS@users.noreply.github.com>
2023-01-20 18:23:46 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const BlogPage = ({ data }) => {
 					data.allMdx.nodes.map(node => (
 						<article key={node.id}>
 							<h2>
-								<Link to={`blog/${node.frontmatter.slug}`}>
+								<Link to={`/blog/${node.frontmatter.slug}`}>
 									{node.frontmatter.title}
 								</Link>
 							</h2>
